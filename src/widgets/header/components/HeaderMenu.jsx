5cf8ce7f56d04f311b077a197aa388b3edf86410
Amange1di <amangeldi2702006@gmail.com>
2025-01-25 16:31:02 +0600

    modified:   src/widgets/header/components/HeaderMenu.jsx

--- src/widgets/header/components/HeaderMenu.jsx
+++ src/widgets/header/components/HeaderMenu.jsx
@@ -1,79 +1,4 @@
-// import { useState, useCallback } from "react";
-// import { NavLink } from "react-router-dom";
 
-// export const headerMenu = [
-//   { name: "ГЛАВНАЯ", path: "/home" },
-//   { name: "О РАЙОНЕ", path: "/about" },
-//   {
-//     name: "АДМИНИСТРАЦИЯ", path: "/administration",
-//     subMenu: [
-//       { name: "руководители", path: "/" },
-//       { name: "структура администрации", path: "/" },
-//       { name: "вакансии", path: "/" },
-//     ],
-//   },
-//   { name: "НОВОСТИ", path: "/news" },
-//   {
-//     name: "ОБРАЩЕНИЯ ГРАЖДАН", path: "/requests",
-//     subMenu: [
-//       { name: "Авторизоваться", path: "/" },
-//       { name: "Продолжить без авторизации", path: "/" },
-//       { name: "администрация", path: "/" },
-//     ],
-//   },
-//   { name: "ПРОЕКТЫ", path: "/projects" },
-//   {
-//     name: "АНТИКОРРУПЦИОННЫЕ МЕРОПРИЯТИЯ", path: "/events"
-//   },
-//   { name: "ГАЛЕРЕЯ", path: "/gallery" },
-// ];
-
-// const HeaderMenu = () => {
-//   const [activeMenu, setActiveMenu] = useState(null);
-//   const [timeoutId, setTimeoutId] = useState(null);
-
-//   const handleMouseEnter = useCallback((name) => {
-//     if (timeoutId) {
-//       clearTimeout(timeoutId);
-//     }
-//     setActiveMenu(name);
-//   }, [timeoutId]);
-
-//   const handleMouseLeave = useCallback(() => {
-//     const id = setTimeout(() => {
-//       setActiveMenu(null);
-//     }, 300);
-//     setTimeoutId(id);
-//   }, []);
-
-//   return (
-//     <div className="header_menu">
-//       {headerMenu.map((page) => (
-//         <div
-//           key={page.name}
-//           className="menu_item"
-//           onMouseEnter={() => page.subMenu && handleMouseEnter(page.name)}
-//           onMouseLeave={handleMouseLeave}
-//         >
-//           <NavLink to={page.path} className="link">
-//             {page.name}
-//           </NavLink>
-//           {page.subMenu && activeMenu === page.name && (
-//             <div className="dropdown_menu">
-//               {page.subMenu.map((subPage) => (
-//                 <NavLink key={subPage.name} to={subPage.path} className="dropdown_link">
-//                   {subPage.name}
-//                 </NavLink>
-//               ))}
-//             </div>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default HeaderMenu;
 import { useState, useCallback } from "react";
 import { NavLink } from "react-router-dom";
 
@@ -81,7 +6,7 @@ export const headerMenu = [
   { name: "ГЛАВНАЯ", path: "/home" },
   { name: "О РАЙОНЕ", path: "/about" },
   {
-    name: "АДМИНИСТРАЦИЯ", path: "/administration",
+    name: "АДМИНИСТРАЦИЯ", 
     subMenu: [
       { name: "руководители", path: "/" },
       { name: "структура администрации", path: "/" },
@@ -90,7 +15,7 @@ export const headerMenu = [
   },
   { name: "НОВОСТИ", path: "/news" },
   {
-    name: "ОБРАЩЕНИЯ ГРАЖДАН", path: "/requests",
+    name: "ОБРАЩЕНИЯ ГРАЖДАН",
     subMenu: [
       { name: "Авторизоваться", path: "/" },
       { name: "Продолжить без авторизации", path: "/" },
